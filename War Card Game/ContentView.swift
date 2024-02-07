3704bd18a504d30ed7e697b0d0264a42d9da3e25
MaxiMacBook <maximgb@hotmail.com>
2024-02-07 20:33:34 -0300

    Haptic feedback added for button pressed action

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,15 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card7"
+    @State var cpuCard = "card2"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+//                Image("button")
+                Button(){
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(/*@START_MENU_TOKEN@*/.bottom, 10.0/*@END_MENU_TOKEN@*/)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+
     }
+    
+    func deal() {
+        //Randomize the players cards
+        let playerCardValue = Int.random(in: 2...14)
+        let cpuCardValue = Int.random(in: 2...14)
+        
+        //Update the player card
+        playerCard = "card" + String(playerCardValue)
+        
+        //Update the cpu card
+        cpuCard = "card" + String(cpuCardValue)
+        
+        //Update scores
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        } else if playerCardValue < cpuCardValue {
+            cpuScore += 1
+        }
+        
+        let impactLight = UIImpactFeedbackGenerator(style: .rigid)
+        impactLight.impactOccurred()
+    }
+    
 }
 
 #Preview {
